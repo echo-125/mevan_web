64ff2522f1df7ff87140c4134b79d3eb0f089988
echo125 <a1258477832@qq.com>
2020-01-17 11:42:10 +0800

    Initial commit

---
+++ src/main/java/com/he/Servlet/MyServlet.java
@@ -0,0 +1,23 @@
+package com.he.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+        request.getRequestDispatcher("index.jsp").forward(request,response);
+        response.getWriter().println(new Date().toString());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("doPost");
+    }
+}
